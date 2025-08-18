e281ad67e9d980465a90ac81f2591a0bb57e4d9e
anwar1827 <anwarhossen1827@gmail.com>
2025-08-18 08:58:48 +0600

    finL

--- db.php
+++ db.php
@@ -4,11 +4,8 @@ $user = "root";
 $password = "";
 $database = "hishabify_db";
 
-
-//$conn = new mysqli("sql112.byethost31.com", "b31_39634701", "009251Aa@", "b31_39634701_hishabify_db");
-$conn = new mysqli($host, $user, $pass, $database);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
